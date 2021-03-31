0700ade90c45974e323cbadaa749a079f5bf2c69
CynicalSorcerer <martin.heathcote@gmail.com>
2021-03-31 20:48:48 +0100

    .

--- buttons.py
+++ buttons.py
@@ -9,7 +9,7 @@ key2 = Button(6)
 key3 = Button(13)
 key4 = Button(19)
 
-key1.when_pressed = say_hello(1)
+key1.when_pressed = say_hello(5)
 key2.when_pressed = say_hello(6)
 key3.when_pressed = say_hello(13)
 key4.when_pressed = say_hello(19)
