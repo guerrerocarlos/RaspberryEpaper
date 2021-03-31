06a3eae2c201f2a5fca25b3c9222d4cfdd47e3f8
CynicalSorcerer <martin.heathcote@gmail.com>
2021-03-31 20:45:03 +0100

    added buttons file

---
+++ buttons.py
@@ -0,0 +1,17 @@
+from gpiozero import Button
+from signal import pause
+
+def say_hello(button_number):
+    print("Button: " + button_number)
+
+key1 = Button(5)
+key2 = Button(6)
+key3 = Button(13)
+key4 = Button(19)
+
+key1.when_pressed = say_hello(1)
+key2.when_pressed = say_hello(6)
+key3.when_pressed = say_hello(13)
+key4.when_pressed = say_hello(19)
+
+pause()
