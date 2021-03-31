6cfef475344055e7f2d9c1b874559bfa1ad23d68
CynicalSorcerer <martin.heathcote@gmail.com>
2021-03-31 20:52:34 +0100

    Changed button handler

--- buttons.py
+++ buttons.py
@@ -1,17 +1,30 @@
 from gpiozero import Button
 from signal import pause
 
-def say_hello(button_number):
-    print("Button: " + str(button_number))
+def handleBtnPress(btn):
+    # get the button pin number
+    pinNum = btn.pin.number
+
+    # python hack for a switch statement. The number represents the pin number and
+    # the value is the message we will print
+    switcher = {
+        5: "Hello, World!",
+        6: "This is my first \nRPi project.",
+        13: "Hope you liked it.",
+        19: "Goodbye"
+    }
+
+     msg = switcher.get(btn.pin.number, "Error")
+     print(msg)
 
 key1 = Button(5)
 key2 = Button(6)
 key3 = Button(13)
 key4 = Button(19)
 
-key1.when_pressed = say_hello(5)
-key2.when_pressed = say_hello(6)
-key3.when_pressed = say_hello(13)
-key4.when_pressed = say_hello(19)
+key1.when_pressed = handleBtnPress
+key2.when_pressed = handleBtnPress
+key3.when_pressed = handleBtnPress
+key4.when_pressed = handleBtnPress
 
 pause()
