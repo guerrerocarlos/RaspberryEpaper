48ad5177b2a9f9f10f70425aa2e3d60f173e5906
CynicalSorcerer <martin.heathcote@gmail.com>
2021-03-31 21:25:21 +0100

    combined screen and button

--- main.py
+++ main.py
@@ -8,15 +8,43 @@ if os.path.exists(libdir):
 from waveshare_epd import epd2in7b
 from PIL import Image
 import time
+from gpiozero import Button
+from signal import pause
 
-epd = epd2in7b.EPD()
-epd.init()
-epd.Clear()
-time.sleep(1)
+def clearScreen():
+    epd = epd2in7b.EPD()
+    epd.init()
+    epd.Clear()
+    time.sleep(1)
 
-HBlackimage = Image.open(os.path.join(picdir, '2in7b-b.bmp'))
-HRedimage = Image.open(os.path.join(picdir, '2in7b-r.bmp'))
-epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
-time.sleep(2)
+def colourScreen():
+    HBlackimage = Image.open(os.path.join(picdir, '2in7b-b.bmp'))
+    HRedimage = Image.open(os.path.join(picdir, '2in7b-r.bmp'))
+    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
+    time.sleep(2)
 
-epd.sleep()
+def exitProgram():
+    epd.sleep()
+    sys.exit()
+
+def handleButton(button)
+    if button.pin == 5:
+        print("Clearing screen..")
+        clearScreen()
+    elif button.pin == 6:
+        print("Colour image...")
+        colourScreen():
+    else:
+        def exitProgram():
+
+key1 = Button(5)
+key2 = Button(6)
+key3 = Button(13)
+key4 = Button(19)
+
+key1.when_pressed = handleButton
+key2.when_pressed = handleButton
+key3.when_pressed = handleButton
+key4.when_pressed = handleButton
+
+pause()
